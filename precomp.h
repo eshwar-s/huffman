7e460e485ea98a4b89c19b6df06dea3ef2646b1c
eshwar-s <70458803+eshwar-s@users.noreply.github.com>
2022-03-08 15:41:08 -0800

    Update precomp.h

--- precomp.h
+++ precomp.h
@@ -9,12 +9,4 @@
 #include <sstream>
 #include <map>
 
-#define abs(x) ((x) < 0 ? -(x) : (x))
-
-int gcd(int a, int b);
-int max(int a, int b);
-void swap(int* i, int* j);
-void swap(char* arr, int i, int j);
-void print_array(int arr[], int n);
-void print_2d_array(int* arr, int row, int col);
-void print_string(const char* str, int low, int high);
+#define abs(x) ((x) < 0 ? -(x) : (x)
